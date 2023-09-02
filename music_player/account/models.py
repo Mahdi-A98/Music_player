a0a88cc06990f05b47b0ad24c1a95a67a4afe556
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-09-02 18:06:08 +0330

    chore: making nase model for user

--- music_player/account/models.py
+++ music_player/account/models.py
@@ -4,13 +4,19 @@ from django.utils.translation import gettext_lazy as _
 from django.utils.html import mark_safe
 # Create your models here.
 
-class User(AbstractUser):
-    bio = models.TextField(null=True, blank=True)
-    email = models.EmailField(_("email address"), null=True, blank=True)
-    image = models.ImageField(verbose_name=_("Product Image"), upload_to='profile/images/')
-    date_joined = models.DateTimeField(verbose_name=_("Joined Date"), auto_now_add=True)
+
+class BaseUserModel(AbstractUser):
+    image = models.ImageField(verbose_name=_("Product Image"), null=True, blank=True,  upload_to='profile/images/')
     last_modify = models.DateTimeField(verbose_name=_("Last Modify"), auto_now=True)
 
+    class Meta:
+        abstract = True
+
     def img_preview(self):
-        if self.photo:
-            return mark_safe(f'<img src = "{self.photo.url}" width = "150" height="150"/> ')
+        if self.image:
+            return mark_safe(f'<img src = "{self.image.url}" width = "150" height="150"/> ')
+
+class User(BaseUserModel):
+    email = models.EmailField(_("email address"), null=True, blank=True)
+    account_type = models.CharField(max_length=20, choices=(("N", "Normal"), ("V", "VIP")))
+    
