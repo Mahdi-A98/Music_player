f140e1d5407bf1ecf000c315e37f8af061cc86c8
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-09-02 18:08:25 +0330

    chore: registering account models in admin panel

--- music_player/account/admin.py
+++ music_player/account/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import User, Artist, Band
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Band)
+admin.site.register(Artist)
