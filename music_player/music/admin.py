0bf1891e6d9afa118a1192d60f7bbd175b1858f4
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-09-02 18:11:25 +0330

    chore: registering music models in admin panel

--- music_player/music/admin.py
+++ music_player/music/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import PlayList, Song, Gener
 # Register your models here.
+
+admin.site.register(PlayList)
+admin.site.register(Song)
+admin.site.register(Gener)
