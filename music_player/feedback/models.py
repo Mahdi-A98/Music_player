eddd55c7e23e8d491614115cbfcfc2368a520112
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-09-02 18:10:09 +0330

    chore: using setting auth user in models

--- music_player/feedback/models.py
+++ music_player/feedback/models.py
@@ -1,15 +1,19 @@
 from django.db import models
+from django.conf import settings
+from django.utils.translation import gettext_lazy as _
+
+from core.models import BaseModel
 from music.models import Song
 from account.models import User
-from core.models import BaseModel
 
 # Create your models here.
 class Like(BaseModel):
     # _id = models.BigIntegerField(primary_key=True) 
     song = models.ForeignKey(Song, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
 class Comment(BaseModel):
     text = models.TextField(_("Comment"))
     song = models.ForeignKey(Song, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    
