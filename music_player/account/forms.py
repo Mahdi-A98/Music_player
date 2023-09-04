4bf3360a629f294a84aaa52f576369e8deba0271
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-09-04 15:29:01 +0330

    chore: login form

--- music_player/account/forms.py
+++ music_player/account/forms.py
@@ -66,3 +66,8 @@ class UserRegisterForm(forms.ModelForm):
         if commit:
             user.save()
         return user
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField(required=True, min_length=4, max_length=50, label='Username or Email',
+                               widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(required=True, min_length=8, max_length=50, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
