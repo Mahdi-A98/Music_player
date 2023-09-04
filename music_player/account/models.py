b9b55ed8a28547abbce09deaab168d51fa8a88b9
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-09-04 10:06:15 +0330

    chore: removing base model for user

--- music_player/account/models.py
+++ music_player/account/models.py
@@ -5,21 +5,16 @@ from django.utils.html import mark_safe
 # Create your models here.
 
 
-class BaseUserModel(AbstractUser):
+class User(AbstractUser):
     image = models.ImageField(verbose_name=_("Product Image"), null=True, blank=True,  upload_to='profile/images/')
+    email = models.EmailField(_("email address"),unique=True, null=True, blank=True)
+    account_type = models.CharField(max_length=20, choices=(("N", "Normal"), ("V", "VIP")))
     last_modify = models.DateTimeField(verbose_name=_("Last Modify"), auto_now=True)
-
-    class Meta:
-        abstract = True
 
     def img_preview(self):
         if self.image:
             return mark_safe(f'<img src = "{self.image.url}" width = "150" height="150"/> ')
 
-class User(BaseUserModel):
-    email = models.EmailField(_("email address"), null=True, blank=True)
-    account_type = models.CharField(max_length=20, choices=(("N", "Normal"), ("V", "VIP")))
-    
 
 class Band(models.Model):
     name = models.CharField(max_length=50)
