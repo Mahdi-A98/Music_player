9b8e21c18e4b43ca7e9cfd16b03cc4e1c1078828
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-09-05 19:07:47 +0330

    chore: Home view

--- music_player/music/views.py
+++ music_player/music/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.contrib.auth import login, logout
 
+from account.authentication import MyUserBackend
+from account.models import User
+from .forms import CreatePlayListForm
+
+
+from .models import Song, PlayList, Gener
 # Create your views here.
+class Home(ListView):
+    model = Song
+    template_name = 'music/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["playlists"] = PlayList.objects.filter(owner=self.request.user) if isinstance(self.request.user, User) else []
+        context["playlist_form"] = CreatePlayListForm()
+        return context
+    
+
+    # def dispatch(self, request, *args, **kwargs):
+    #     if request.user.is_authenticated:
+    #         print('home view: user is logged in')
+    #     else:
+    #         print('home view: user is  not logged in')
+
+    #     # print(self.request.user)
+    #     # print(type(self.request.user))
+    #     return super().dispatch(request, *args, **kwargs)
+    
